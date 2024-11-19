8df7ceb3909bcee0014df2033cc640c5fea651ed
Ozan Yazıcı <ozanyazici9@gmail.com>
2024-11-19 15:44:56 +0300

    AndroidFeatureUiConventionPlugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -41,5 +41,9 @@ gradlePlugin {
             id = "runique.android.library.compose"
             implementationClass = "AndroidLibraryComposeConventionPlugin"
         }
+        register("androidFeatureUi") {
+            id = "runique.android.feature.ui"
+            implementationClass = "AndroidFeatureUiConventionPlugin"
+        }
     }
 }
